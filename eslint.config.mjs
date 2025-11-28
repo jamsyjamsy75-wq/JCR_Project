8ab4fe8dfa7819efc8bef533a3630c6fbcbe5e70
jamsyjamsy75-wq <tchibba.jamal@gmail.com>
2025-11-28 14:51:41 +0100

    fix: disable strict react-hooks eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,12 @@
 import next from "eslint-config-next";
 
-export default [...next];
+export default [
+  ...next,
+  {
+    rules: {
+      "react-hooks/set-state-in-effect": "off",
+    },
+  },
+];
 
 
